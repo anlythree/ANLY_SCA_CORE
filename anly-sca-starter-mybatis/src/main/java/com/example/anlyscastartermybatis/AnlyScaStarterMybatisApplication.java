f7842adb059e4e1a1e11b1e06b9b9f03d0d6c71d
anlythree <15610462126@163.com>
2023-07-09 12:08:04 +0800

    code sync

---
+++ anly-sca-starter-mybatis/src/main/java/com/example/anlyscastartermybatis/AnlyScaStarterMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.anlyscastartermybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnlyScaStarterMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnlyScaStarterMybatisApplication.class, args);
+    }
+
+}
---
+++ anly-sca-starter-mybatis/src/test/java/com/example/anlyscastartermybatis/AnlyScaStarterMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anlyscastartermybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnlyScaStarterMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
