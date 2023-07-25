6cc5d14fc6459b48879c962062dfa315b92a10a1
anlythree <15610462126@163.com>
2023-07-25 11:49:04 +0800

    bugfix

--- anly-sca-starter-mysql/src/main/java/com/anlythree/anlymysql/AnlyScaStarterMysqlApplication.java
+++ anly-sca-starter-mysql/src/main/java/com/anlythree/anlymysql/AnlyScaStarterMysqlApplication.java
@@ -1,6 +1,5 @@
 package com.anlythree.anlymysql;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
