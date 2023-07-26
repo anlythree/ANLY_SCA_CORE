732a7797ce1b5c93364b54391da51218d1e516d2
anlythree <15610462126@163.com>
2023-07-26 09:58:11 +0800

    code sync

--- anly-sca-starter-mysql/src/main/java/com/anlythree/anlymysql/config/mybatis/MybatisPlusConfiguration.java
+++ anly-sca-starter-mysql/src/main/java/com/anlythree/anlymysql/config/mybatis/MybatisPlusConfiguration.java
@@ -3,27 +3,18 @@ package com.anlythree.anlymysql.config.mybatis;
 import com.anlythree.anlymysql.config.mybatis.injector.AnlySqlInjector;
 import com.anlythree.anlymysql.config.mybatis.interceptor.SqlLogInterceptor;
 import com.anlythree.anlymysql.config.props.AnlyMybatisProperties;
-import com.anlythree.anlymysql.config.props.TenantProperties;
 import com.anlythree.common.factory.YamlPropertySourceFactory;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import lombok.AllArgsConstructor;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * mybatis plus配置中心
@@ -37,7 +28,7 @@ import java.util.List;
 @EnableTransactionManagement
 @EnableConfigurationProperties(AnlyMybatisProperties.class)
 @PropertySource(factory = YamlPropertySourceFactory.class, value = "classpath:anly-mybatisplus.yml")
-@MapperScan("com.anlythree.**.mapper.**")
+@MapperScan({"com.anlythree.**.mapper.**","com.anlythree.anlymysql.business.*.dao"})
 public class MybatisPlusConfiguration {
 
 
