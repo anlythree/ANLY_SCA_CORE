7c2cb5b00de6740c5ba9f4d4edc01e970274ef8b
anlythree <15610462126@163.com>
2023-07-28 10:32:26 +0800

    添加全局异常处理

--- anly-sca-starter-common/src/main/java/com/anlythree/common/exception/AnlyScaException.java
+++ anly-sca-starter-common/src/main/java/com/anlythree/common/exception/AnlyScaException.java
@@ -1,5 +1,8 @@
 package com.anlythree.common.exception;
 
+import com.anlythree.common.api.ResultCode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -9,9 +12,13 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
  *
  * @author pangu
  */
+@Data
+@EqualsAndHashCode(callSuper=true)
 public class AnlyScaException extends RuntimeException {
 
 	private static final long serialVersionUID = 5782968730281544562L;
+
+	private ResultCode errorCode;
 
 	private int status = INTERNAL_SERVER_ERROR.value();
 
@@ -19,8 +26,8 @@ public class AnlyScaException extends RuntimeException {
 		super(message);
 	}
 
-	public AnlyScaException(HttpStatus status, String message) {
+	public AnlyScaException(ResultCode errorCode, String message) {
 		super(message);
-		this.status = status.value();
+		this.errorCode = errorCode;
 	}
 }
