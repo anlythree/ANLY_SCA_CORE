18d4ee17e3ea1acf2c27da99fc06b3bc5b0e233b
anlythree <15610462126@163.com>
2023-07-31 15:03:24 +0800

    code sync

---
+++ anly-sca-starter-kafka/src/main/java/com/anlythree/anlykafka/AnlyScaStarterKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.anlythree.anlykafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnlyScaStarterKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnlyScaStarterKafkaApplication.class, args);
+	}
+
+}
---
+++ anly-sca-starter-kafka/src/test/java/com/anlythree/anlyscastarterkafka/AnlyScaStarterKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anlythree.anlyscastarterkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnlyScaStarterKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
