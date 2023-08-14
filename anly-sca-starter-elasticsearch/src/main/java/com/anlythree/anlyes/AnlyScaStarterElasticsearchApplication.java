5faf8eec5bd317c3467668d4d4028f6570b23f1e
anlythree <15610462126@163.com>
2023-08-14 10:35:30 +0800

    code sync

---
+++ anly-sca-starter-elasticsearch/src/main/java/com/anlythree/anlyes/AnlyScaStarterElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.anlythree.anlyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnlyScaStarterElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnlyScaStarterElasticsearchApplication.class, args);
+	}
+
+}
---
+++ anly-sca-starter-elasticsearch/src/test/java/com/anlythree/anlyes/AnlyScaStarterElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anlythree.anlyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnlyScaStarterElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
